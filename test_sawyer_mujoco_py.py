88707b92ec2ab9b65a27906830f410863ebf4951
Tarik Kelestemur <tkelestemur@gmail.com>
2018-11-15 15:36:34 -0500

    added sampling space

--- test_sawyer_mujoco_py.py
+++ test_sawyer_mujoco_py.py
@@ -9,14 +9,27 @@ MODEL_XML_PATH = PATH + '/model/sawyer_reach.xml'
 model = mj.load_model_from_path(MODEL_XML_PATH)
 sim = mj.MjSim(model)
 
-viewer = mj.MjViewer(sim)
-
-# sim.data.qpos[0] = 0.1
+# viewer = mj.MjViewer(sim)
+sim.reset()
+# qpos = np.zeros(7)
+# qvel = np.zeros(7)
+# qpos[1] = 0.5
+# old_state = sim.get_state()
+# new_state = mj.MjSimState(old_state.time, qpos, qvel,
+#                                  old_state.act, old_state.udd_state)
+# sim.set_state(new_state)
 # sim.forward()
-# sim.model.opt.timestep = 0.001
-sim.data.ctrl[:] = np.zeros(7)
-sim.data.ctrl[0] = 0.1
-for i in range(50000):
-    sim.step()
-    print ("Joint 0 Vel: {}".format(sim.data.qvel[0]))
-    viewer.render()
+#
+# sim.data.ctrl[:] = np.zeros(7)
+# sim.data.ctrl[1] = 0.1
+# i = 0
+# for i in range(20):
+#
+#     sim.step()
+#     i += 1
+#     viewer.render()
+#     print ("Joint 1 Vel: {}".format(sim.data.qvel[1]))
+
+# while True:
+#     sim.step()
+#     viewer.render()
