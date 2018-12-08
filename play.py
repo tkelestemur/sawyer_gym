fbf2963dfe6a24d1c7e0e4d3493a8f10968bdbc0
Tarik Kelestemur <tkelestemur@gmail.com>
2018-12-07 22:28:24 -0500

    upload results

--- play.py
+++ play.py
@@ -9,20 +9,23 @@ SAVE_PATH = PATH + '/results/sawyer/ppo'
 
 
 def play():
-    _, get_action = load_policy(SAVE_PATH)
-    env = SawyerGraspEnv(n_substeps=25)
+    env, get_action = load_policy(SAVE_PATH)
+    # env = SawyerGraspEnv(n_substeps=1)
     n_episode = 100
     ep_len, ep_ret, i = 0, 0, 0
-    max_ep_len = 500
+    max_ep_len = 5000
     obs = env.reset()
 
     while i < n_episode:
         action = get_action(obs)
-        # print('Action: {}'.format(action))
-        obs, r, d, _ = env.step(action)
+        print('Action: {}'.format(action))
+        obs, r, d, reward_info = env.step(action)
         ep_len += 1
         ep_ret += r
-        env.render()
+        print('dist_reward: {} grasp_reward: {} terminal_reward: {}'.format(reward_info['dist_reward'],
+                                                                            reward_info['grasp_reward'],
+                                                                            reward_info['terminal_reward']))
+        # env.render()
         if d or (ep_len == max_ep_len):
             print('DONE: Episode Length: {} Episode Reward {}'.format(ep_len, ep_ret))
             obs = env.reset()
