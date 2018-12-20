5e2a564d77de52544b173434f1891879e625fbed
Tarik Kelestemur <tkelestemur@gmail.com>
2018-12-20 14:05:58 -0500

    baseline ppo update

--- train_baseline.py
+++ train_baseline.py
@@ -1,4 +1,6 @@
+import os
 import tensorflow as tf
+from gym.wrappers import TimeLimit
 from baselines.ppo2 import ppo2
 from baselines import logger
 from baselines.common.tf_util import get_session
@@ -9,11 +11,16 @@ from envs.sawyer_env import SawyerGraspEnv
 
 env = SawyerGraspEnv(n_substeps=5)
 
+env = TimeLimit(env, max_episode_steps=1000)
+
 env = DummyVecEnv([lambda: env])
 env = VecNormalize(env)
 
+PATH = os.path.dirname(os.path.realpath(__file__))
+SAVE_PATH = os.path.join(PATH, 'results', 'baselines', 'ppo')
+
 save_dir = "/home/tarik/sawyer_gym/results/baselines"
-model_name = "sawyer_grasp_ppo"
+model_name = "/grasp/ppo"
 
 
 def train(save=False):
